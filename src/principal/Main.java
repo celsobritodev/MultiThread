86e1ed36f4bb7d73cc54cf76b2ce94ec61f2f79a
celsobritodev <celsinhobrito@gmail.com>
2025-08-15 10:12:57 -0300

    segundo commit

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -3,20 +3,30 @@ package principal;
 public class Main {
 
 	public static void main(String[] args) {
+		
 		// thread atual
 		Thread t = Thread.currentThread();
 		System.out.println(t.getName());
-		
+
 		// nova thread
-		Thread t1 = new Thread(new MeuRunnable());
-		t1.start(); // executando em uma nova thread
+		MeuRunnable meuRunnable = new MeuRunnable();
+		Thread t1 = new Thread(meuRunnable);
 		
+
 		// Runnable como lambda
 		Thread t2 = new Thread(() -> {
+			System.out.println(Thread.currentThread().getName());
 			System.out.println("Inscreva-se no canal!");
 		});
+		
+		// t2.start(); // NAÕ FAÇA ISSO, lança IllegalThreadStateException
+
+		// nova thread
+		Thread t3 = new Thread(meuRunnable);
+		
+		t1.start(); // executando em uma nova thread
 		t2.start(); // executando em uma nova thread
-		//t2.start(); // NAÕ FAÇA ISSO, lança IllegalThreadStateException
+		t3.start(); // executando em uma nova thread
 	}
 
 }
